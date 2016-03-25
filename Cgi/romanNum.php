13a812154729e1170a2c71ca7325ecc8cc4b2e3f
MarvellousChester <PhantomX16@mail.ru>
2016-03-25 13:33:51 +0200

    Roman numbers

--- Cgi/romanNum.php
+++ Cgi/romanNum.php
@@ -3,47 +3,52 @@
 
 class test
 {
-    protected $mas = array('M' => 1000, 'D' => 500, 'C' => 100, 'L' => 50, 'X' => 10, 'V' => 5, 'I' => 1);
+    /**
+     * @var array $mas The massive of major values
+     */
+    protected $mas
+        = array('M' => 1000, 'D' => 500, 'C' => 100, 'L' => 50, 'X' => 10,
+                'V' => 5, 'I' => 1);
     protected $romannum = '';
     protected $result;
+
     function convert($value)
     {
-        if($value > 3999) {
+        //Cant calc more then 3999
+        if ($value > 3999) {
             return false;
         }
-            $remainder = (int)($value / current($this->mas));
-            if ($remainder == 0) {
-                if (!next($this->mas)) {
-                    $this->result = $this->romannum;
-                } else {
-                    $this->convert($value);
-                }
-            }
-            elseif(substr($value, 0, 1) == 9) {
-                next($this->mas);
-                $this->romannum .= key($this->mas);
-                $value -= current($this->mas) * 9;
-                prev($this->mas);
-                prev($this->mas);
-                $this->romannum .= key($this->mas);
-                next($this->mas);
-                next($this->mas);
-                $this->convert($value);
-            }
-            elseif ($remainder == 4) {
-                $this->romannum .= key($this->mas);
-                prev($this->mas);
-                $this->romannum .= key($this->mas);
-                next($this->mas);
-                $value -= current($this->mas) * 4;
-
-                $this->convert($value);
-
+        $remainder = (int)($value / current($this->mas));
+        if ($remainder == 0) {
+            if (!next($this->mas)) {
+                $this->result = $this->romannum;
             } else {
-                $this->romannum .= key($this->mas);
-                $value -= current($this->mas);
                 $this->convert($value);
             }
+        } elseif (substr($value, 0, 1) == 9) {
+            next($this->mas);
+            $this->romannum .= key($this->mas);
+            $value -= current($this->mas) * 9;
+            prev($this->mas);
+            prev($this->mas);
+            $this->romannum .= key($this->mas);
+            next($this->mas);
+            next($this->mas);
+            $this->convert($value);
+        } elseif ($remainder == 4) {
+            $this->romannum .= key($this->mas);
+            prev($this->mas);
+            $this->romannum .= key($this->mas);
+            next($this->mas);
+            $value -= current($this->mas) * 4;
+
+            $this->convert($value);
+
+        } else {
+            $this->romannum .= key($this->mas);
+            $value -= current($this->mas);
+            $this->convert($value);
+        }
 
 
         return $this->result;
