573c7036c88c42db20a36d1281cae335b4880292
MarvellousChester <PhantomX16@mail.ru>
2016-03-25 13:39:06 +0200

    Roman numbers

--- Cgi/romanNum.php
+++ Cgi/romanNum.php
@@ -4,7 +4,7 @@
 class test
 {
     /**
-     * @var array $mas The massive of major values
+     * @var array $mas The massive of digits values
      */
     protected $mas
         = array('M' => 1000, 'D' => 500, 'C' => 100, 'L' => 50, 'X' => 10,
@@ -12,19 +12,35 @@ class test
     protected $romannum = '';
     protected $result;
 
+    /**
+     * @param $value
+     *
+     * @return bool|string
+     */
     function convert($value)
     {
         //Cant calc more then 3999
         if ($value > 3999) {
             return false;
         }
+
+        /** @var int $remainder
+         * Remainder of the division
+         */
         $remainder = (int)($value / current($this->mas));
+        /**
+         * If reminder is zero move to next digits value
+         */
         if ($remainder == 0) {
             if (!next($this->mas)) {
                 $this->result = $this->romannum;
             } else {
                 $this->convert($value);
             }
+
+            /**
+             * Else check for 9 first
+             */
         } elseif (substr($value, 0, 1) == 9) {
             next($this->mas);
             $this->romannum .= key($this->mas);
@@ -35,6 +51,10 @@ class test
             next($this->mas);
             next($this->mas);
             $this->convert($value);
+
+            /**
+             * Next for 4
+             */
         } elseif ($remainder == 4) {
             $this->romannum .= key($this->mas);
             prev($this->mas);
@@ -44,6 +64,9 @@ class test
 
             $this->convert($value);
 
+            /**
+             * End finally all other variants
+             */
         } else {
             $this->romannum .= key($this->mas);
             $value -= current($this->mas);
